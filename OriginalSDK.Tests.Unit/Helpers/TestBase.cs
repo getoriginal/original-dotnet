0e40789f5f3f3e22834140e8352e0e705e19bf57
dondemetrius <dondemetrius7@gmail.com>
2024-11-07 11:21:26 +0000

    Add missing sale price in USD. Delete .env files

--- OriginalSDK.Tests.Unit/Helpers/TestBase.cs
+++ OriginalSDK.Tests.Unit/Helpers/TestBase.cs
@@ -9,6 +9,11 @@ namespace OriginalSDK.Tests.Unit.Helpers
 
     private static void LoadEnvironmentVariables()
     {
+      Environment.SetEnvironmentVariable("ORIGINAL_API_KEY", null);
+      Environment.SetEnvironmentVariable("ORIGINAL_API_SECRET", null);
+      Environment.SetEnvironmentVariable("ORIGINAL_BASE_URL", null);
+      Environment.SetEnvironmentVariable("ORIGINAL_ENVIRONMENT", null);
+
       var root = Directory.GetCurrentDirectory();
       var dotenv = Path.Combine(root, ".env.test.unit");
       DotNetEnv.Env.TraversePath().Load(dotenv);
